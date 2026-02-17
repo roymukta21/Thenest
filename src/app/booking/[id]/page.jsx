db58701c8fa3f3fdf3376ae5fb6700d2875fd96c
MUKTA ROY <muktaroy520@gmail.com>
2026-02-17 16:37:16 +0600

    2nd commit

--- src/app/booking/[id]/page.jsx
+++ src/app/booking/[id]/page.jsx
@@ -1,9 +1,10 @@
+import MyBookingsPage from "@/app/my-bookings/page";
 import PrivateRoute from "@/components/PrivateRoute";
 
 export default function BookingWrapper(props) {
   return (
     <PrivateRoute>
-      <BookingPage {...props} />
+      <MyBookingsPage {...props} />
     </PrivateRoute>
   );
 }
